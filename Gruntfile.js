b24f510f63f9815071c21274fe82569918c730c6
kristoferjoseph <kristoferjoseph@gmail.com>
2013-06-29 10:34:25 -0700

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+/**
+ *
+ * Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+module.exports = function(grunt) {
+    'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                '<%= simplemocha.all %>'
+                ],
+            options: {
+                jshintrc: '.jshintrc',
+            },
+        },
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp']
+        },
+
+        topdoc: {
+            usageguides: {
+                options: {
+                    source: 'test/fixtures/',
+                    destination: 'tmp/output/',
+                    template: 'http://github.com/topcoat/usage-guide-theme',
+                    templateData: '<%= pkg.topdoc %>'
+                }
+            }
+        },
+
+        // Unit tests.
+        simplemocha: {
+            options: {
+                ui: 'bdd',
+                reporter: 'Nyan'
+            },
+            all: ['test/*.test.js'],
+        },
+
+    });
+
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-simple-mocha');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'topdoc', 'simplemocha']);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/topdoc.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+module.exports = function(grunt) {
+    'use strict';
+
+    var Topdoc = require('topdoc');
+
+    grunt.registerMultiTask('topdoc', 'Generates a usage guide using Topdoc from a commented css file', function() {
+
+        var options = this.options({}),
+            topdoc = new Topdoc(options);
+
+        console.log('OPTIONS:', options);
+
+        //TODO: Errors?
+        topdoc.generate();
+        grunt.log.writeln('Generated usage guides at ' + options.destination);
+
+    });
+};
